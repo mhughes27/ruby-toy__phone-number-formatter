8f53696812e72f924aa49b10ad84a49fbbfea73b
Matt Hughes <mhughes27@gmail.com>
2016-06-27 15:49:41 -0500

    Tests pass

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,11 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  return phone_number_str
+ 
+
+filtered = phone_number_str.gsub(/[^0-9]/,'')
+arr = filtered.chars
+
+
+return "(#{arr[0]}#{arr[1]}#{arr[2]}) #{arr[3]}#{arr[4]}#{arr[5]}-#{arr[6]}#{arr[7]}#{arr[8]}#{arr[9]}"
 end
